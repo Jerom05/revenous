2ba06ac95cb91c967d0e6ea84a524e74652e97e0
Jerom05 <jeromghagra@gmail.com>
2020-05-21 23:34:18 +0600

    Updates

---
+++ precache-manifest.574a974217b098b1102006d7fd76999f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebdd1d15cb2a89919500a2c70e44321a",
+    "url": "/revenous/index.html"
+  },
+  {
+    "revision": "6bde3aafc6fe7a818457",
+    "url": "/revenous/static/css/main.0530dabf.chunk.css"
+  },
+  {
+    "revision": "afc11979210a9a53aa60",
+    "url": "/revenous/static/js/2.09f78244.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/revenous/static/js/2.09f78244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bde3aafc6fe7a818457",
+    "url": "/revenous/static/js/main.48ab5f51.chunk.js"
+  },
+  {
+    "revision": "d0b57d59d79aaeaba6f1",
+    "url": "/revenous/static/js/runtime-main.3f616b48.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/revenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/revenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
